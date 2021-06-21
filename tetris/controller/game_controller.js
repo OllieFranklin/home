7c1c796555bc0dac36932c263a5c2bbbd6b54c34
Ollie Franklin <ollie.franklin3@gmail.com>
2021-06-21 17:49:54 +1200

    Write board renderer, and hook it up to controller.
    Game now runs, although there are currently no inputs.

--- tetris/controller/game_controller.js
+++ tetris/controller/game_controller.js
@@ -1,29 +1,55 @@
 // instantiates a game, gives it inputs
 // reads outputs, and provides them to the view
 
-let game;
+class GameController {
 
-window.onload = function () {
+    constructor(game, boardRenderer) {
+        this.game = game;
+        this.boardRenderer = boardRenderer;
 
-	game = new Game(29);
+        const me = this;
+        this.interval = setInterval(function () {
+            me.step();
+        }, 1000/60);
 
-    // create a new Game
-    // attach eventListeners to the view 
-    // send inputs to the game
-    // recieve outputs from the game
-    // call functions in view with these outputs
+        // console.log(this.game);
 
-    // for (let i =0 ; i < 130; i++) {
-    // 	game.nextFrame(false, false, false, false, false);
-    // }
-    printFrame(false, false, false, false, false);
-    // printFrame(false, true, false, false, false);
-    // printFrame(false, true, false, false, false);
-    // printFrame(false, true, false, false, false);
+        // setInterval(this.step, 1000/60);
 
+        // this.state = game.nextFrame(false, false, false, false, false);
+        // this.draw(this.state);
+        
+        // for (let i =0 ; i < 130; i++) {
+        //  game.nextFrame(false, false, false, false, false);
+        // }
+        // printFrame(false, false, false, false, false);
+        // printFrame(false, true, false, false, false);
+        // printFrame(false, true, false, false, false);
+        // printFrame(false, true, false, false, false);
+
+    }
+
+    step() {
+        const state = this.game.nextFrame(false, false, false, false, false);
+        this.boardRenderer.draw(state);
+    }
 }
 
-function printFrame(up, left, right, rotateCW, rotateACW) {
-	const state = game.nextFrame(up, left, right, rotateCW, rotateACW);
-	console.log(state);
-}
+// let gameController;
+
+// window.onload = function () {
+
+	
+
+//     // create a new Game
+//     // attach eventListeners to the view 
+//     // send inputs to the game
+//     // recieve outputs from the game
+//     // call functions in view with these outputs
+
+// }
+
+// function printFrame(up, left, right, rotateCW, rotateACW) {
+// 	const state = game.nextFrame(up, left, right, rotateCW, rotateACW);
+// 	console.log(state);
+// }
--- tetris/view/board_renderer.js
+++ tetris/view/board_renderer.js
@@ -1,22 +1,44 @@
-// draws the board
 class BoardRenderer {
 
 	constructor() {
-		const canvas = document.getElementById("board");
-		this.ctx = canvas.getContext("2d");
+		this.canvas = document.getElementById("board");
+		this.ctx = this.canvas.getContext("2d");
+
+		const tetriminoTypes = ["I", "J", "L", "O", "S", "T", "Z"];
+		this.textureMap = new Map();
+		for (let i = 0; i < tetriminoTypes.length; i++) {
+			const texture = document.getElementById(tetriminoTypes[i] + "_Tetromino_default");
+			this.textureMap.set(tetriminoTypes[i], texture);
+		}
 	}
 
 	draw(gameState) {
 
-		// const boardState = gameState.board;
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-		// for (row of board) {
-		// 	for (col of board) {
-				
-		// 	}
-		// }
+		const boardState = gameState.board;
+
+		const rows = boardState.length;
+		const cols = boardState[0].length;
+
+		const cellWidth = this.canvas.width / cols;
+		const cellHeight = this.canvas.height / rows;
+
+		for (let row = 0; row < rows; row++) {
+			
+			const y = row * cellHeight;
+
+		    for (let col = 0; col < cols; col++) {
+
+		    	const x = col * cellWidth; 
+
+		    	const str = boardState[row][col]
+		    	if (this.textureMap.has(str)) {
+		    		this.ctx.drawImage(this.textureMap.get(str), x, y, cellWidth, cellHeight);
+		    	}
+		    }
+		}
 
 	}
-
 
 }
